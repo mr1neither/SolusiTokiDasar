860de236796e8e9622f7b9d71745f8cef62ce593
Bryna Adhyasta <brinazela@gmail.com>
2023-09-19 12:26:55 +0700

    array 9d

---
+++ 9d.cpp
@@ -0,0 +1,25 @@
+#include<cstdio>
+
+int main(){
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int arr[n][m];
+	for (int i=0; i<n; i++){
+		for (int j=0; j<m; j++){
+			scanf("%d", &arr[i][j]);       	
+		}
+	}
+	printf("\n");
+	for (int i=0; i<m; i++){
+		for (int j=n-1; j>=0; j--){
+			printf("%d", arr[j][i]);
+			if (j>0){
+				printf(" ");
+			}
+			else {
+				printf("\n");
+			}
+		}
+	}
+
+}
